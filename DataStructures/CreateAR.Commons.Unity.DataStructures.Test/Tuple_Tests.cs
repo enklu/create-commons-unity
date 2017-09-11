10d66b5c2a4e5f30efec03660507809434d92f5f
Benjamin Jordan <bmj465@gmail.com>
2017-09-10 19:26:51 -0700

    Fixing tests.

---
+++ DataStructures/CreateAR.Commons.Unity.DataStructures.Test/Tuple_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CreateAR.Commons.Unity.DataStructures.Test
+{
+    [TestFixture]
+    public class Tuple_Tests
+    {
+        [Test]
+        public void Create()
+        {
+            var a = 1;
+            var b = Guid.NewGuid().ToString();
+
+            var tuple = Tuple.Create(a, b);
+
+            Assert.AreEqual(a, tuple.Item1);
+            Assert.AreEqual(b, tuple.Item2);
+        }
+    }
+}
